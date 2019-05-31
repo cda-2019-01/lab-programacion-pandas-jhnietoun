f12610bcd8b57c03aae69be208b1e97b9f6158a0
Jairo Hernando Nieto Hernández <jnietoh@unal.edu.co>
2019-05-31 12:36:13 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -2,5 +2,8 @@
 ## Imprima los valores unicos e la columna _c4 de 
 ## de la tabla tbl1 en mayusculas
 ## 
-
-
+import pandas as pd
+data=pd.read_csv('tbl1.tsv',delimiter='\t', encoding='utf-8')
+data=data.apply(lambda x: x.astype(str).str.upper())
+data=data ['_c4'].unique()
+print (sorted (data))
