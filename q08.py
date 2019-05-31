52204b403a9177167483447629c91dce91b81e2d
Jairo Hernando Nieto Hernández <jnietoh@unal.edu.co>
2019-05-31 12:36:29 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -1,4 +1,7 @@
 ##
 ## Agregue el año como una columna a la tabla tbl0.tsv 
 ## 
-
+import pandas as pd
+data=pd.read_csv('tbl0.tsv',delimiter='\t', encoding='utf-8')
+data['ano'] = data._c3.apply(lambda x: x[:4])
+print (data)
