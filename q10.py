69b3b5a5e899fbd1ae75707feb4c6533035689b6
Jairo Hernando Nieto Hernández <jnietoh@unal.edu.co>
2019-06-08 13:19:44 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,21 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
+import pandas as pd
 
+df = pd.read_csv('tbl1.tsv', delimiter='\t')
+group = df.groupby('_c0')
+data = []
+
+for i in group:
+    letters = sorted(list(i[1]['_c4']))
+    number = i[0]
+    letters_string = str(letters[0])
+
+    for j in range(1, len(letters)):
+        letters_string += ',%s' % (str(letters[j]))
+
+    data.append([number, letters_string])
+
+df = pd.DataFrame(data=data, columns=['_c0', 'lista'])
+print(df)
